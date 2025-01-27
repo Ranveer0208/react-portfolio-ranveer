9b164df2fb750b6dfdfcf7646ee72bc95c29b247
Ranveer0208 <ranveerproject2@gmail.com>
2025-01-28 00:59:25 +0530

    Updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ const Navbar = () => {
       <div className="nav flex items-center justify-between px-10 md:px-20 py-6 w-full  text-white">
          <div className="left text-lg md:text-3xl">RK</div>
          <div className="right flex md:space-x-6 space-x-4 hover:cursor-pointer text-sm md:text-2xl">
-            <div><a href="https://www.linkedin.com/in/ranveer-varada-679018165" className="hover:text-blue-500"><FaLinkedin className="s" /></a></div>
-            <div><a href="https://github.com/Ranveer0208/" className="hover:text-slate-600"><FaGithub /></a></div>
+            <div><a href="https://www.linkedin.com/in/ranveer-varada-679018165" target="_blank" className="hover:text-blue-500"><FaLinkedin className="s" /></a></div>
+            <div><a href="https://github.com/Ranveer0208/" target="_blank" className="hover:text-slate-600"><FaGithub /></a></div>
             <div><a href=""><FaInstagram /></a></div>
             <div><a href=""><FaXTwitter /></a></div>
          </div>
