e323905c9a6356900aa9e80b7d6b778c263028d0
Ranveer0208 <ranveerproject2@gmail.com>
2025-01-28 16:07:12 +0530

    Updated file

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -6,6 +6,7 @@ import { FaPhp } from "react-icons/fa6";
 import { IoLogoCss3, IoLogoJavascript } from "react-icons/io5";
 import { TbBrandNextjs } from "react-icons/tb";
 import { AiOutlineHtml5 } from "react-icons/ai";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 // Define the variants for the animation
 const iconVariants = (duration: number): Variants => ({
@@ -30,65 +31,132 @@ const Technology = () => {
                variants={iconVariants(2.5)} // Pass the variants prop
                initial="initial"
                animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <RiReactjsLine className="text-6xl text-cyan-400" />
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+                  <RiReactjsLine className="text-6xl text-cyan-400" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] right-0 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">React
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-8 rotate-45"></div>
+                  </div>
+               </span>
+
+            </motion.div>
+
+            <motion.div
+               variants={iconVariants(3.2)} // Pass the variants prop
+               initial="initial"
+               animate="animate"
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+
+                  <IoLogoJavascript className="text-6xl text-yellow-500" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] -right-5 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">JavaScript
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-12 rotate-45"></div>
+                  </div>
+               </span>
             </motion.div>
             <motion.div
                variants={iconVariants(3.2)} // Pass the variants prop
                initial="initial"
                animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <IoLogoJavascript className="text-6xl text-yellow-500" />
-            </motion.div>
-            <motion.div
-               variants={iconVariants(3.2)} // Pass the variants prop
-               initial="initial"
-               animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <BiLogoTypescript className="text-6xl text-blue-500" />
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+
+                  <BiLogoTypescript className="text-6xl text-blue-500" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] -right-5 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">TypeScript
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-12 rotate-45"></div>
+                  </div>
+               </span>
+
+
             </motion.div>
             <motion.div
                variants={iconVariants(3)} // Pass the variants prop
                initial="initial"
                animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <TbBrandNextjs className="text-6xl" />
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+                  <TbBrandNextjs className="text-6xl" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] -right-3 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">Nextjs
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-8 rotate-45"></div>
+                  </div>
+               </span>
+
             </motion.div>
             <motion.div
                variants={iconVariants(4)} // Pass the variants prop
                initial="initial"
                animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <FaPhp className="text-6xl text-purple-500" />
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+                  <FaPhp className="text-6xl text-purple-500" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] -right-3 scale-0 group-hover:scale-100 bg-neutral-800 px-5 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">PHP
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-8 rotate-45"></div>
+                  </div>
+               </span>
+
             </motion.div>
             <motion.div
                variants={iconVariants(2.7)} // Pass the variants prop
                initial="initial"
                animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <GrMysql className="text-6xl text-orange-600" />
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+
+
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+                  <GrMysql className="text-6xl text-orange-600" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] -right-3 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">MySql
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-8 rotate-45"></div>
+                  </div>
+               </span>
+
+            </motion.div>
+            <motion.div
+               variants={iconVariants(3.7)} // Pass the variants prop
+               initial="initial"
+               animate="animate"
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+
+
+               <span className="group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+                  <RiTailwindCssFill className="text-6xl text-blue-400" />
+                  <div className=" w-[10rem] absolute top-[7rem] -right-3 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 text-center">Tailwind Css
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-8 rotate-45"></div>
+                  </div>
+               </span>
+
             </motion.div>
             <motion.div
                variants={iconVariants(3.2)} // Pass the variants prop
                initial="initial"
                animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <AiOutlineHtml5 className="text-6xl text-amber-600" />
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+
+
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+                  <AiOutlineHtml5 className="text-6xl text-amber-600" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] -right-3 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">HTML
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-8 rotate-45"></div>
+                  </div>
+               </span>
+
             </motion.div>
             <motion.div
                variants={iconVariants(2.9)} // Pass the variants prop
                initial="initial"
                animate="animate"
-               className="rounded-2xl border-4 border-neutral-800 p-4"
-            >
-               <IoLogoCss3 className="text-6xl text-blue-600" />
+               className="rounded-2xl border-4 border-neutral-800 p-4">
+
+               <span className="flex flex-wrap group relative rounded-xl hover:bg-neutral-hover:text-cyan-300 hover:cursor-pointer">
+                  <IoLogoCss3 className="text-6xl text-blue-600" />
+                  <div className="flex flex-wrap gap-1 w-fit absolute top-[7rem] -right-3 scale-0 group-hover:scale-100 bg-neutral-800 px-4 py-3 rounded-lg border-2 border-cyan-400 transition-transform duration-300 ease-in-out -translate-x-15 ">CSS
+                     <div className="scale-0 group-hover:scale-100 transition-transform duration-1000 ease-in-out w-5 h-5 bg-neutral-800 border-2 border-t-cyan-400 border-l-cyan-400 border-b-transparent border-r-transparent absolute -top-[0.69rem] right-8 rotate-45"></div>
+                  </div>
+               </span>
+
+
             </motion.div>
          </motion.div>
       </div>
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,14 +2,12 @@ import project1 from "../assets/projects_pic/kuberaquiz.jpeg";
 import project2 from "../assets/projects_pic/ms.jpeg";
 import project3 from "../assets/projects_pic/rentz4u_landing_page.jpeg";
 import project4 from "../assets/projects_pic/vus.jpeg";
-import project5 from "../assets/projects_pic/coronatest.jpeg";
+import project5 from "../assets/projects_pic/portfolio.png"
+import project6 from "../assets/projects_pic/coronatest.jpeg";
 
 export const HERO_CONTENT: string = `Self-motivated developer with over 3 years of hands-on experience working on a wide range of web-based projects. Passionate about coding and specialize in creating customized user interfaces that are accessible, user-friendly, and secure. I am highly organized and able to manage multiple projects and deadlines effectively. By staying up-to-date with the latest technologies, I consistently apply best practices to deliver high-quality solutions. I am also skilled in adapting to new challenges and continuously improving my work to meet the evolving needs of each project.`;
 
-export const ABOUT_TEXT: string = `I am a motivated software developer with over three years of experience building web-based applications, I have honed my skills in crafting intuitive, secure, and scalable solutions that meet users' needs.
-During my tenure at AVRL in Seattle, I contributed to developing advanced bot functionalities using JavaScript, earning two promotions in under two years due to consistently high performance. I also managed multiple client projects simultaneously, ensuring timely delivery and quality. 
-Beyond my professional experience, I’ve initiated and built platforms such as Rentz4u and VuStatus, showcasing my expertise in full-stack development and entrepreneurial acumen. For instance, Rentz4u successfully listed ₹2.6 lakh worth of rental properties in its first month
-.`;
+export const ABOUT_TEXT: string = `I am a motivated software developer with over three years of experience building web-based applications, I have honed my skills in crafting intuitive, secure, and scalable solutions that meet users' needs. During my tenure at AVRL in Seattle, I contributed to developing advanced bot functionalities using JavaScript, earning two promotions in under two years due to consistently high performance. I also managed multiple client projects simultaneously, ensuring timely delivery and quality. Beyond my professional experience, I’ve initiated and built platforms such as Rentz4u and VuStatus, showcasing my expertise in full-stack development and entrepreneurial acumen. For instance, Rentz4u successfully listed ₹2.6 lakh worth of rental properties in its first month.`;
 
 type ExperienceType = {
    year: string;
@@ -30,8 +28,7 @@ export const EXPERIENCES: ExperienceType[] | undefined = [
       • Developed bot functionality using JavaScript as part of the AVRL team, delivering high-quality solutions for various clients.
       • Achieved two promotions within 1.5 years, reflecting strong performance, growth, and leadership potential.
       • Managed multiple clients simultaneously, ensuring timely and effective delivery of bot-related services and solutions.
-      • Dedicated engineer for a key customer for 6-7 months, providing focused support and tailored solutions to meet their specific needs.
-    `,
+      • Dedicated engineer for a key customer for 6-7 months, providing focused support and tailored solutions to meet their specific needs.`,
       technologies: ["Javascript", "Typescript"],
       website: "",
    },
@@ -42,8 +39,7 @@ export const EXPERIENCES: ExperienceType[] | undefined = [
       description: `
       • Initiated and developed Rentz4u, a platform designed to help users find budget-friendly rooms for rent.
       • Successfully listed ₹2.6 lakh worth of houses in the first month of the platform's launch, demonstrating quick growth and market acceptance.
-      • Drove user acquisition and engagement, leading to significant traction and visibility for the platform.
-      `,
+      • Drove user acquisition and engagement, leading to significant traction and visibility for the platform.`,
       technologies: ["HTML", "CSS", "Javascript", "PHP", "mySQL", "AJAX"],
       website: "https://rentz4u.vustatus.com"
    },
@@ -53,8 +49,7 @@ export const EXPERIENCES: ExperienceType[] | undefined = [
       company: "VuStatus app, Ranchi, Jharkhand, India",
       description: `
       • Developed VuStatus, an innovative platform that enables users to check the real-time availability and current status of doctors.•Enabled live appointment tracking, allowing patients to monitor appointment statuses and reducing wait times.
-      • Enhanced patient experience by providing transparent, up-to-date information on doctor availability and appointment schedules.
-      `,
+      • Enhanced patient experience by providing transparent, up-to-date information on doctor availability and appointment schedules.`,
       technologies: ["HTML", "CSS", "Javascript", "PHP", "mySQL", "AJAX"],
       website: "https://business.vustatus.com"
    },
@@ -66,6 +61,8 @@ type ProjectType = {
    image: string;
    description: string;
    technologies: string[];
+   demo_link: string;
+   source_code?: string,
 }
 
 export const PROJECTS: ProjectType[] = [
@@ -75,6 +72,7 @@ export const PROJECTS: ProjectType[] = [
       image: project1,
       description: `A live multiplayer quiz game based on the theme "Invite, Play, Earn, and Learn".`,
       technologies: ["HTML", "CSS", "Javascript", "PHP", "mySQL", "AJAX"],
+      demo_link: "https://kuberaquiz.vustatus.com/",
    },
    {
       id: 2,
@@ -82,6 +80,8 @@ export const PROJECTS: ProjectType[] = [
       image: project2,
       description: `Microsoft's home page clone using Typescript and Tailwind css.`,
       technologies: ["React", "TypeScript", "Tailwind", "HTML", "CSS"],
+      demo_link: "https://react-microsoft-clone-mu.vercel.app/",
+      source_code: "https://github.com/Ranveer0208/react-microsoft-clone",
    },
    {
       id: 3,
@@ -89,6 +89,7 @@ export const PROJECTS: ProjectType[] = [
       image: project3,
       description: `a platform designed to help users find budget-friendly rooms for rent.`,
       technologies: ["HTML", "CSS", "Javascript", "PHP", "mySQL", "AJAX"],
+      demo_link: "https://rentz4u.vustatus.com/",
    },
    {
       id: 4,
@@ -96,13 +97,25 @@ export const PROJECTS: ProjectType[] = [
       image: project4,
       description: `A platform that enables users to check the real-time availability & current status of doctors.`,
       technologies: ["React", "TypeScript", "Tailwind", "HTML", "CSS"],
+      demo_link: "https://business.vustatus.com/",
    },
    {
       id: 5,
-      title: "CoronaTest Website",
+      title: "Portfolio Website",
       image: project5,
+      description: `A personal portfolio website showcasing projects, skills, and contact information.`,
+      technologies: ["React", "TypeScript", "Tailwind", "HTML", "CSS"],
+      demo_link: "https://react-portfolio-ranveer.vercel.app/",
+      source_code: "https://github.com/Ranveer0208/react-portfolio-ranveer",
+   },
+   {
+      id: 6,
+      title: "CoronaTest Website",
+      image: project6,
       description: `It is a simple website,  which provids a quick self-assessment tool for COVID-19 risk.`,
       technologies: ["React", "TypeScript", "Tailwind", "HTML", "CSS"],
+      demo_link: "https://coronatest.vustatus.com/",
+      source_code: "https://github.com/Ranveer0208/coronatest",
    },
 ];
 
