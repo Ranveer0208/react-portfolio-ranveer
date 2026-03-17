6c072757f3acb3d21abe572e4ae23febd91b4d33
Ranveer0208 <ranveerproject2@gmail.com>
2026-03-17 13:36:41 +0530

    updated project

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -84,7 +84,7 @@ export const PROJECTS: ProjectType[] = [
       id: 7,
       title: "Aadivan Foundation Website & Admin Dashboard",
       image: project7,
-      description: `Developed a responsive NGO website along with a secure admin dashboard for content management. Implemented role-based access, protected routes, and dynamic CRUD features for managing reports, partners, awards, and activities.`,
+      description: `Developed a responsive NGO website along with a secure admin dashboard for content management.`,
       technologies: ["React", "JavaScript", "Tailwind", "PHP", "MySQL"],
       demo_link: "https://aadivanfoundation.org/",
       source_code: "",
